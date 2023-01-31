2e550b85f37061c576f7ff2661d32011b02b76af
Chandra Shekhar Shukla <chandrashekhar.s@STLERI.COM>
2023-02-01 00:46:08 +0530

    added implementation

--- diff.java
+++ diff.java
@@ -1,3 +1,11 @@
+import java.util.*;
 public class diff {
-    
+ 
+    public static void main(String[] args){
+        int a =66;
+        int b = 33;
+        System.out.println("the difference of numbers is " + (a-b));
+    }
 }
+    
+
